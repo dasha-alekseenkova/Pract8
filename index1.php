22e7b7d3cbb764805a53fb338bde1b254e7bd56f
dasha-alekseenkova <72573782+dasha-alekseenkova@users.noreply.github.com>
2020-11-13 23:55:03 +0200

    Update index1.php

--- index1.php
+++ index1.php
@@ -1,43 +1,15 @@
+<!DOCTYPE html>
 <html>
-<head>
-<title>Test</title>
-</head>
 <body>
+
 <?php
-// function search($s, $row, $col){
-
-// for($i=0;$i<$row;$i++){
-// for($j=0;$j<$col;$j++)
-// echo $s[$i][$j] . " ";
-// echo "<br>";
-// }
-// }
-
-// $a = array();
-
-// $r = rand(1,10);
-// $c = rand(1,10);
-
-// for($i=0;$i<$r;$i++)
-// for($j=0;$j<$c;$j++)
-// $a[$i][$j]= rand(1,10);
-
-
-// arr($a, $r, $c);
-$arr = array(1, 2, 3, 4, 5, 6);
-for($i = 0; $i < $snum; $i++)
-{
-   $symbol = $arr[rand(0, count($arr)];
-   if($i % 3)
-   {
-      echo"{$symbol}</ br>";
-   }
-   else
-   {
-      echo $symbol;
-   }
-}
-echo"<br>"
+$a=array("red","green","blue","yellow","brown");
+$random_keys=array_rand($a,3);
+echo $a[$random_keys[0]]."<br>";
+echo $a[$random_keys[1]]."<br>";
+echo $a[$random_keys[2]];
 ?>
+
 </body>
-</html> 
+</html>
+
