313b71315f10efa82aee088b4c9df1efcfdf229e
dasha-alekseenkova <72573782+dasha-alekseenkova@users.noreply.github.com>
2020-11-14 10:46:16 +0200

    Update index1.php

--- index1.php
+++ index1.php
@@ -3,13 +3,31 @@
 <body>
 
 <?php
-$a=array("red","green","blue","yellow","brown");
-$random_keys=array_rand($a,3);
-echo $a[$random_keys[0]]."<br>";
-echo $a[$random_keys[1]]."<br>";
-echo $a[$random_keys[2]];
-?>
+define("N",10);
+define("M",5);
+$array = [];
+ 
 
+ 
+for ( $i = 0; $i < N; $i++ ) {
+    for ($j = 0; $j < M; $j++) {
+        $array[$i][$j] = rand( 1, 9 );        
+    }
+}
+ 
+
+ 
+echo "<table>";
+foreach ($array as $result){
+        echo "<tr>";
+        foreach ($result as $rValue){
+                echo "<td>{$rValue}</td>";
+        }
+        echo "</tr>";
+}
+echo "</table>";
+ 
+?>
 </body>
 </html>
 
